ad065a8a266896526d45a14e03429758e85661cb
suhan8984 <suhan8984@daum.net>
2025-04-01 09:25:37 +0900

    feat: rnn

--- _common/optimizer.py
+++ _common/optimizer.py
@@ -1,12 +1,16 @@
 import numpy as np
 
 class SGD:
-	def __init__(self, lr=0.01):
-		self.lr = lr
+    def __init__(self, lr=0.01):
+        self.lr = lr
 
-	def update(self, params, grads):
-		for key in params.keys():
-			params[key] -= self.lr * grads[key]
+    def update(self, params, grads):
+        if isinstance(params, dict):
+            for key in params.keys():
+                params[key] -= self.lr * grads[key]
+        elif isinstance(params, list):
+            for i in range(len(params)):
+                params[i] -= self.lr * grads[i]
 
 
 class Momentum:
